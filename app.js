c44db282dc01d5032149f1eb443085edb8d326a2
qujialin <qujialin52@hotmail.com>
2018-08-08 23:04:22 +0800

    add the function of language switching

--- app.js
+++ app.js
@@ -10,27 +10,21 @@ var http = require('http');
 var path = require('path');
 var querystring = require('querystring');
 var watson = require('watson-developer-cloud');
-
-var conversation = watson.conversation({
-//EN
-// username: '339a3675-d104-4a05-85eb-d904c5ea435b', // replace with username from service key
-// password: 'LWURiQPWFQQA',
-// path: { workspace_id: '84948310-3f00-4e3b-a0b3-78260ff6168c' },
-
-//JP
-username: "79cd0712-482b-4fb3-8ea3-0710eada5e99",
-password: "g3Ntt7PHu4Ze",
-path: { workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1' },
-version: 'v1',
-version_date: '2017-04-21'
+var conversation;
+var conMode = '1';
+conversation = watson.conversation({
+	//JP
+	username: "79cd0712-482b-4fb3-8ea3-0710eada5e99",
+	password: "g3Ntt7PHu4Ze",
+	path: { workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1' },
+	version: 'v1',
+	version_date: '2017-04-21'
 });
-
 
 var authorization1 = new watson.AuthorizationV1({
 	//EN
-    username: "27d0e5e4-5399-42be-b706-c0172c1afe6a",
+	username: "27d0e5e4-5399-42be-b706-c0172c1afe6a",
 	password: "85ve32lfOOak"
-	
 });
 
 var app = express();
@@ -49,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
@@ -58,98 +52,138 @@ app.get('/users', user.list);
 var toshow = '';
 
 function processResponse(err, response) {
-	  if (err) {
-	    console.error('EROR:'+err); // something went wrong
-	    return;
-	  }
-
-	  // If an intent was detected, log it out to the console.
-	  if (response.intents.length > 0) {
-	    console.log('Detected intent: #' + response.intents[0].intent);
-	  }
-	  
-	  // Display the output from dialog, if any.
-	  if (response.output.text.length != 0) {
-//	      console.log(response.output.text[0]);
-	      toshow = response.output.text;
-	      console.log("************"+toshow);
-	  }
-
+	if (err) {
+		console.error('EROR:' + err); // something went wrong
+		return;
 	}
 
-var context  =  null;
-
-app.post('/mainflow', function(req, res, next) {
-	console.log('$$$$$$$$$$$$$$$$$$ In mainflow:',req.body.input, req.body.leavemgmt);
-	
-	if (null == context) {
-		context = req.body.context;
-
+	// If an intent was detected, log it out to the console.
+	if (response.intents.length > 0) {
+		console.log('Detected intent: #' + response.intents[0].intent);
 	}
-	
-	var url = req.headers.referer;
-	var param = url.split("?");
-	console.log("url:"+url+" type:"+typeof(url)+" length:"+param.length );
-	
-	var username = '';
-	if(param.length == 2)
-	{
-		var params =  querystring.parse(param[1]);
-		if(null != params.username)
-		{
-			username = params.username+',';
-		}
-		console.log("params:"+JSON.stringify(params, null, 4));	
+
+	// Display the output from dialog, if any.
+	if (response.output.text.length != 0) {
+		//	      console.log(response.output.text[0]);
+		toshow = response.output.text;
+		console.log("************" + toshow);
 	}
-	
-	
-	
-	var client_input = req.body.input;
-	
-	conversation.message({
-		 // EN 
-		 // workspace_id: '84948310-3f00-4e3b-a0b3-78260ff6168c',
-		 //JP
-		 workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1',
-		  input: { text: client_input },
-		  context: context
-		 }, function(err, response) {
-		     if (err) {
-		       console.error(err);
-		     } else {
-//		    	 console.log("DDDDDDD:"+context.conversation_id);
-		       console.log(JSON.stringify(response, null, 2));
-		       context = response.context;//多轮对话需要将res的context赋给请求context
-		   	if( null == req.body.input)
-			{
-				// EN
-				// res.json('Hello, I am Watson, How can I help you? (Get more about us in <a href=\'#\' onclick=\'window.open("http://www.ibm.com/watson"); return;\'> IBM Watson</a>)');
-				// JP
-				res.json('こんにちは、私はWatsonですが、お手伝いできますか。? (私たちについてもっと <a href=\'#\' onclick=\'window.open("http://www.ibm.com/watson"); return;\'> IBM Watson</a>)');
-			}
-		   	else
-		   	{
-		   		res.json(response.output.text[0]);
-		   	}
-		     }
+}
+
+//言語切替
+app.get('/getCommMode', function (req, res, next) {
+	conMode = req.query.model;
+	if (conMode == 2) {
+		conversation = watson.conversation({
+			//EN
+			username: '339a3675-d104-4a05-85eb-d904c5ea435b', // replace with username from service key
+			password: 'LWURiQPWFQQA',
+			path: { workspace_id: '84948310-3f00-4e3b-a0b3-78260ff6168c' },
+			version: 'v1',
+			version_date: '2017-04-21'
 		});
-//  	res.json(toshow);
-});
-
-
-app.get('/speech-to-text/token', function(req, res, next) {
-	console.log('-----------speech to text to get token--------------------');
-	authorization1.getToken({
-        url: watson.SpeechToTextV1.URL
-    },function (err, token) {
-        if (!token) {
-            console.log('error:', err);
-        } else {
-           return res.send(token);
-        }
-    });
+	} else {
+		conversation = watson.conversation({
+			//JP
+			username: "79cd0712-482b-4fb3-8ea3-0710eada5e99",
+			password: "g3Ntt7PHu4Ze",
+			path: { workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1' },
+			version: 'v1',
+			version_date: '2017-04-21'
+		});
+		return res.send('this is my commMode');
+	}
 })
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var context = null;
+app.post('/mainflow', function (req, res, next) {
+	console.log('$$$$$$$$$$$$$$$$$$ In mainflow:');
+
+	if (null == context) {
+		context = req.body.context;
+	}
+	var url = req.headers.referer;
+	var param = url.split("?");
+	console.log("url:" + url + " type:" + typeof (url) + " length:" + param.length);
+
+	var username = '';
+	if (param.length == 2) {
+		var params = querystring.parse(param[1]);
+		if (null != params.username) {
+			username = params.username + ',';
+		}
+		console.log("params:" + JSON.stringify(params, null, 4));
+	}
+	var client_input = req.body.input;
+
+	if (conMode == '1') {
+		conversation.message({
+			// EN 
+			// workspace_id: '84948310-3f00-4e3b-a0b3-78260ff6168c',
+			//JP
+			workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1',
+			input: { text: client_input },
+			context: context
+		}, function (err, response) {
+			if (err) {
+				console.error(err);
+			} else {
+				//		    	 console.log("DDDDDDD:"+context.conversation_id);
+				console.log(JSON.stringify(response, null, 2));
+				context = response.context;//多轮对话需要将res的context赋给请求context
+				if (null == req.body.input) {
+					// EN
+					// res.json('Hello, I am Watson, How can I help you? (Get more about us in <a href=\'#\' onclick=\'window.open("http://www.ibm.com/watson"); return;\'> IBM Watson</a>)');
+					// JP
+					res.json('こんにちは、私はWatsonですが、お手伝いできますか。? (チャット言語 <a href=\'#2\' onclick=getCommKbn(\'1\');getCommMode(\'1\')> Japanese</a> Or <a href=\'#2\' onclick=getCommKbn(\'2\');getCommMode(\'2\')> English</a>)');
+				}
+				else {
+					res.json(response.output.text[0]);
+				}
+			}
+		});
+	} else {
+		conversation.message({
+			// EN 
+			workspace_id: '84948310-3f00-4e3b-a0b3-78260ff6168c',
+			//JP
+			// workspace_id: '0734c777-95ed-4200-ae42-869614ec34a1',
+			input: { text: client_input },
+			context: context
+		}, function (err, response) {
+			if (err) {
+				console.error(err);
+			} else {
+				//		    	 console.log("DDDDDDD:"+context.conversation_id);
+				console.log(JSON.stringify(response, null, 2));
+				context = response.context;//多轮对话需要将res的context赋给请求context
+				if (null == req.body.input) {
+					// EN
+					// res.json('Hello, I am Watson, How can I help you? (Get more about us in <a href=\'#\' onclick=\'window.open("http://www.ibm.com/watson"); return;\'> IBM Watson</a>)');
+					// JP
+					res.json('こんにちは、私はWatsonですが、お手伝いできますか。? (チャット言語 <a href=\'#2\' onclick=getCommKbn(\'1\');getCommMode(\'1\')> Japanese</a> Or <a href=\'#2\' onclick=getCommKbn(\'2\');getCommMode(\'2\')> English</a>)');
+				}
+				else {
+					res.json(response.output.text[0]);
+				}
+			}
+		});
+	}
+});
+
+app.get('/speech-to-text/token', function (req, res, next) {
+	console.log('-----------speech to text to get token--------------------');
+	authorization1.getToken({
+		url: watson.SpeechToTextV1.URL
+	}, function (err, token) {
+		if (!token) {
+			console.log('error:', err);
+		} else {
+			return res.send(token);
+		}
+	});
+})
+
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
 });
--- public/js/demo.js
+++ public/js/demo.js
@@ -26,226 +26,249 @@ var inputHistory = [];
 var inputHistoryPointer = -1
 
 var $chatInput = $('.chat-window--message-input'),
-$jsonPanel = $('#json-panel .base--textarea'),
-$information = $('.data--information'),
-$profile = $('.data--profile'),
-$loading = $('.loader');
+  $jsonPanel = $('#json-panel .base--textarea'),
+  $information = $('.data--information'),
+  $profile = $('.data--profile'),
+  $loading = $('.loader');
 //Further changes	
 var $infoConfidence = $('.data--info_confidence');
 
-$chatInput.keyup(function(event){
-if(event.keyCode === 13) {
-  converse($(this).val());
-}
-});
-
-
-$chatInput.keydown(function(event){
-switch(event.which){
-	case 13: // enter
-	  inputHistory.push($chatInput.val());
-	  //inputHistory.push($leaveBtn.val());
-	  inputHistoryPointer = inputHistory.length;
-	break;
-	case 38: // up
-	if(inputHistoryPointer > 0){
-	   inputHistoryPointer--;
-	   $chatInput.val(inputHistory[inputHistoryPointer]);
-	}
-
-	break;
-	case 40: // down
-	if(inputHistory.length > 0 && inputHistoryPointer < inputHistory.length - 1){
-	   inputHistoryPointer++;
-	   $chatInput.val(inputHistory[inputHistoryPointer]);
-	}
-  else if(inputHistoryPointer == inputHistory.length - 1){
-    $chatInput.val("");
-    inputHistoryPointer++;
+$chatInput.keyup(function (event) {
+  if (event.keyCode === 13) {
+    converse($(this).val());
   }
-	break;
-}
-
 });
 
-var speechToText = function() {
-$.get('/speech-to-text/token')
-.done(function onSuccess(token) {
-  console.log(token);
- var stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
-          token: token,                       // Authorization token to use this service, configured from /speech/stt-token.js file
-          continuous: false,                  // False = automatically stop transcription the first time a pause is detected
-          outputElement: document.getElementById('input-text'),       // CSS selector or DOM Element
-          inactivity_timeout: 3,              // Number of seconds to wait before closing input stream
-          format: true,                       // Inhibits errors
-          keepMicrophone: true,               // Avoids repeated permissions prompts in FireFox
-            // model: "en-US_BroadbandModel"
-            model: "ja-JP_BroadbandModel"
-        }); 
-})
-.fail(function(error){
-  talk('WATSON', error.responseJSON ? error.responseJSON.error : error.statusText);
-})
-.always(function always(){
- 
-  $loading.hide();
+
+$chatInput.keydown(function (event) {
+  switch (event.which) {
+    case 13: // enter
+      inputHistory.push($chatInput.val());
+      //inputHistory.push($leaveBtn.val());
+      inputHistoryPointer = inputHistory.length;
+      break;
+    case 38: // up
+      if (inputHistoryPointer > 0) {
+        inputHistoryPointer--;
+        $chatInput.val(inputHistory[inputHistoryPointer]);
+      }
+
+      break;
+    case 40: // down
+      if (inputHistory.length > 0 && inputHistoryPointer < inputHistory.length - 1) {
+        inputHistoryPointer++;
+        $chatInput.val(inputHistory[inputHistoryPointer]);
+      }
+      else if (inputHistoryPointer == inputHistory.length - 1) {
+        $chatInput.val("");
+        inputHistoryPointer++;
+      }
+      break;
+  }
+});
+
+var speechToText = function () {
+  var conMode = $('#conMode').val();
+  if (conMode == '1') {
+    sttCommon('ja-JP_BroadbandModel');
+  } else {
+    sttCommon('en-US_BroadbandModel');
+  }
+}
+//stt api 共通
+var sttCommon = function (mode) {
+  $.get('/speech-to-text/token')
+    .done(function onSuccess(token) {
+      console.log(token);
+      var stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+        token: token,                       // Authorization token to use this service, configured from /speech/stt-token.js file
+        continuous: false,                  // False = automatically stop transcription the first time a pause is detected
+        outputElement: document.getElementById('input-text'),       // CSS selector or DOM Element
+        inactivity_timeout: 3,              // Number of seconds to wait before closing input stream
+        format: true,                       // Inhibits errors
+        keepMicrophone: true,               // Avoids repeated permissions prompts in FireFox
+        model: mode
+      });
+    })
+    .fail(function (error) {
+      talk('WATSON', error.responseJSON ? error.responseJSON.error : error.statusText);
+    })
+    .always(function always() {
+      $loading.hide();
+      scrollChatToBottom();
+      $chatInput.focus();
+    });
+}
+
+
+//选择语言模式
+var getCommMode = function () {
+  $.get('/getCommMode?model=' + $('#conMode').val())
+    .done(function onSuccess() {
+
+    });
+}
+
+var getCommKbn = function (usertXt) {
+  if (usertXt == '2') {
+    talk('WATSON', 'You have switched to english conversation mode'); // show
+    $('#conMode').val('2');
+  } else {
+    talk('WATSON', '日本語チャットモードに切り替えました。'); // show
+    $('#conMode').val('1');
+  }
   scrollChatToBottom();
-  $chatInput.focus();
-});
 }
+var converse = function (userText) {
+  console.log("In converse:", userText);
 
-var converse = function(userText) {
-	console.log("In converse:", userText);
-	
-$loading.show();
-// $chatInput.hide();
+  $loading.show();
+  // $chatInput.hide();
 
-// check if the user typed text or not
-if (typeof(userText) !== undefined && $.trim(userText) !== '')
-  submitMessage(userText);
+  // check if the user typed text or not
+  if (typeof (userText) !== undefined && $.trim(userText) !== '')
+    submitMessage(userText);
 
-// build the conversation parameters
-var params = { input : userText };
+  // build the conversation parameters
+  var params = { input: userText };
 
-// check if there is a conversation in place and continue that
-// by specifing the conversation_id and client_id
-if (conversation_id) {
-  params.conversation_id = conversation_id;
-  params.client_id = client_id;
-}
+  // check if there is a conversation in place and continue that
+  // by specifing the conversation_id and client_id
+  if (conversation_id) {
+    params.conversation_id = conversation_id;
+    params.client_id = client_id;
+  }
 
-//call NLC here first
-//If we get something not related to BYOD then return
+  //call NLC here first
+  //If we get something not related to BYOD then return
 
-$.post('/mainflow', params)
-  .done(function onSuccess(dialog) {
-	$chatInput.val(''); // clear the text input
-	var response =  dialog;
-	if(dialog.conversation)
-		$jsonPanel.html(JSON.stringify(dialog.conversation, null, 2));
+  $.post('/mainflow', params)
+    .done(function onSuccess(dialog) {
+      $chatInput.val(''); // clear the text input
+      var response = dialog;
+      if (dialog.conversation)
+        $jsonPanel.html(JSON.stringify(dialog.conversation, null, 2));
 
-    // update conversation variables
-	if(dialog.conversation)
-	{
-		conversation_id = dialog.conversation.conversation_id;
-		client_id = dialog.conversation.client_id;
-		var texts = dialog.conversation.response;
-		response = texts.join('<br/>'); // &lt;br/&gt; is <br/>
-	}
-    $chatInput.show();
-    $chatInput[0].focus();
+      // update conversation variables
+      if (dialog.conversation) {
+        conversation_id = dialog.conversation.conversation_id;
+        client_id = dialog.conversation.client_id;
+        var texts = dialog.conversation.response;
+        response = texts.join('<br/>'); // &lt;br/&gt; is <br/>
+      }
+      $chatInput.show();
+      $chatInput[0].focus();
 
-    $information.empty();
-    $infoConfidence.empty();
-    
-	if(dialog.conversation)
-	{
-		addProperty($information, 'Dialog ID: ', dialog.dialog_id);
-		addProperty($information, 'Conversation ID: ', conversation_id);
-		addProperty($information, 'Client ID: ', client_id);
-		addProperty($infoConfidence, 'Confidence: ', dialog.confidence);
-	}
-	talk('WATSON', response); // show
-	
-	if(dialog.conversation)
-		getProfile();
-  })
-  .fail(function(error){
+      $information.empty();
+      $infoConfidence.empty();
+
+      if (dialog.conversation) {
+        addProperty($information, 'Dialog ID: ', dialog.dialog_id);
+        addProperty($information, 'Conversation ID: ', conversation_id);
+        addProperty($information, 'Client ID: ', client_id);
+        addProperty($infoConfidence, 'Confidence: ', dialog.confidence);
+      }
+      talk('WATSON', response); // show
+
+      if (dialog.conversation)
+        getProfile();
+    })
+    .fail(function (error) {
+      talk('WATSON', error.responseJSON ? error.responseJSON.error : error.statusText);
+    })
+    .always(function always() {
+
+      $loading.hide();
+      scrollChatToBottom();
+      $chatInput.focus();
+    });
+
+};
+
+var getProfile = function () {
+  var params = {
+    conversation_id: conversation_id,
+    client_id: client_id
+  };
+
+  $.post('/profile', params).done(function (data) {
+    $profile.empty();
+    data.name_values.forEach(function (par) {
+      if (par.value !== '')
+        addProperty($profile, par.name + ':', par.value);
+    });
+  }).fail(function (error) {
     talk('WATSON', error.responseJSON ? error.responseJSON.error : error.statusText);
-  })
-  .always(function always(){
-	 
-    $loading.hide();
-    scrollChatToBottom();
-    $chatInput.focus();
   });
-
 };
 
-var getProfile = function() {
-var params = {
-  conversation_id: conversation_id,
-  client_id: client_id
+var scrollChatToBottom = function () {
+  var element = $('.chat-box--pane');
+  element.animate({
+    scrollTop: element[0].scrollHeight
+  }, 420);
 };
 
-$.post('/profile', params).done(function(data) {
-  $profile.empty();
-  data.name_values.forEach(function(par) {
-    if (par.value !== '')
-      addProperty($profile, par.name + ':', par.value);
-  });
-}).fail(function(error){
-  talk('WATSON', error.responseJSON ? error.responseJSON.error : error.statusText);
-});
-};
-
-var scrollChatToBottom = function() {
-var element = $('.chat-box--pane');
-element.animate({
-  scrollTop: element[0].scrollHeight
-}, 420);
-};
-
-var scrollToInput = function() {
+var scrollToInput = function () {
   var element = $('.chat-window--message-input');
   $('body, html').animate({
     scrollTop: (element.offset().top - window.innerHeight + element[0].offsetHeight) + 20 + 'px'
   });
 };
 
-function getTagContent(text, tagName){
+function getTagContent(text, tagName) {
 
-var linkRegexp = new RegExp("<"+tagName+">(((?!<\/"+tagName+">).)*)</"+tagName+">", "g")
-var arr = [];
-var matchedLinks;
+  var linkRegexp = new RegExp("<" + tagName + ">(((?!<\/" + tagName + ">).)*)</" + tagName + ">", "g")
+  var arr = [];
+  var matchedLinks;
 
-while(matchedLinks = linkRegexp.exec(text)){
-arr.push(matchedLinks[1]);
-}
-return arr;
-}
-
-function replaceMctLinksByHtmlLinks(text){
-var text = text;
-var linksContent = getTagContent(text, "mct:link");
-
-linksContent.forEach(function(item){
-var url = getTagContent(item, "mct:url")[0];
-var label = getTagContent(item, "mct:label")[0];
-
-var link = "<a href=\""+url+"\" target=\"_blank\">"+label+"</a>";
-
-var regExp = new RegExp("<mct:link>"+item+"</mct:link>", "g");
-
-text = text.replace(regExp, link);
-
-});
-
-return text;
+  while (matchedLinks = linkRegexp.exec(text)) {
+    arr.push(matchedLinks[1]);
+  }
+  return arr;
 }
 
-function replaceAutolearnItemsByButtons(text){
-var output = text;
-var itemsContent = getTagContent(text, "mct:autolearnitems");
+function replaceMctLinksByHtmlLinks(text) {
+  var text = text;
+  var linksContent = getTagContent(text, "mct:link");
 
-itemsContent.forEach(function(itemGroup){
-var itemsText = getTagContent(itemGroup, "mct:item");
+  linksContent.forEach(function (item) {
+    var url = getTagContent(item, "mct:url")[0];
+    var label = getTagContent(item, "mct:label")[0];
 
-itemsText.forEach(function(innerText){
-    var regExp = new RegExp("<mct:item>"+innerText+"</mct:item>", "g");
-    var button = "<button class=\"wds-input\">"+innerText+"</button><br/>";
-    output = output.replace(regExp, button);
-});
+    var link = "<a href=\"" + url + "\" target=\"_blank\">" + label + "</a>";
 
-});
+    var regExp = new RegExp("<mct:link>" + item + "</mct:link>", "g");
 
-output = output.replace(/<mct:autolearnitems>/g, "");
-output = output.replace(/<\/mct:autolearnitems>/g, "");
+    text = text.replace(regExp, link);
 
-return output;
+  });
+
+  return text;
 }
 
-var postProcessOutput = function(text) {
+function replaceAutolearnItemsByButtons(text) {
+  var output = text;
+  var itemsContent = getTagContent(text, "mct:autolearnitems");
+
+  itemsContent.forEach(function (itemGroup) {
+    var itemsText = getTagContent(itemGroup, "mct:item");
+
+    itemsText.forEach(function (innerText) {
+      var regExp = new RegExp("<mct:item>" + innerText + "</mct:item>", "g");
+      var button = "<button class=\"wds-input\">" + innerText + "</button><br/>";
+      output = output.replace(regExp, button);
+    });
+
+  });
+
+  output = output.replace(/<mct:autolearnitems>/g, "");
+  output = output.replace(/<\/mct:autolearnitems>/g, "");
+
+  return output;
+}
+
+var postProcessOutput = function (text) {
   var output = text.replace(/<mct:input>/g, "<button class=\"wds-input\">");
   output = output.replace(/<\/mct:input>/g, "</button>");
   output = replaceMctLinksByHtmlLinks(output);
@@ -253,56 +276,56 @@ var postProcessOutput = function(text) {
   return output;
 };
 
-var talk = function(origin, text) {
-var $chatBox = $('.chat-box--item_' + origin).first().clone();
-var $loading = $('.loader');
-var text = postProcessOutput(text);
-$chatBox.find('p').html($('<p/>').html(text).html());
-// $('.chat-box--pane').append($chatBox);
-$chatBox.insertBefore($loading);
-setTimeout(function() {
-  $chatBox.removeClass('chat-box--item_HIDDEN');
-}, 100);
+var talk = function (origin, text) {
+  var $chatBox = $('.chat-box--item_' + origin).first().clone();
+  var $loading = $('.loader');
+  var text = postProcessOutput(text);
+  $chatBox.find('p').html($('<p/>').html(text).html());
+  // $('.chat-box--pane').append($chatBox);
+  $chatBox.insertBefore($loading);
+  setTimeout(function () {
+    $chatBox.removeClass('chat-box--item_HIDDEN');
+  }, 100);
 
-$chatBox.find('button.wds-input').click(function(){
-  var text = $(this).text();
-  converse(text);
-  inputHistory.push(text);
-  inputHistoryPointer = inputHistory.length;
-});
+  $chatBox.find('button.wds-input').click(function () {
+    var text = $(this).text();
+    converse(text);
+    inputHistory.push(text);
+    inputHistoryPointer = inputHistory.length;
+  });
 
 };
 
-var addProperty = function($parent, name, value) {
-var $property = $('.data--variable').last().clone();
-$property.find('.data--variable-title').text(name);
-$property.find('.data--variable-value').text(value);
-$property.appendTo($parent);
-setTimeout(function() {
-  $property.removeClass('hidden');
-}, 100);
+var addProperty = function ($parent, name, value) {
+  var $property = $('.data--variable').last().clone();
+  $property.find('.data--variable-title').text(name);
+  $property.find('.data--variable-value').text(value);
+  $property.appendTo($parent);
+  setTimeout(function () {
+    $property.removeClass('hidden');
+  }, 100);
 };
 
-var submitMessage = function(text) {
-talk('YOU', text);
-scrollChatToBottom();
-clearInput();
+var submitMessage = function (text) {
+  talk('YOU', text);
+  scrollChatToBottom();
+  clearInput();
 };
 
-var clearInput = function() {
-$('.chat-window--message-input').val('');
+var clearInput = function () {
+  $('.chat-window--message-input').val('');
 };
 
-$('.tab-panels--tab').click(function(e){
-e.preventDefault();
-var self = $(this);
-var inputGroup = self.closest('.tab-panels');
-var idName = null;
+$('.tab-panels--tab').click(function (e) {
+  e.preventDefault();
+  var self = $(this);
+  var inputGroup = self.closest('.tab-panels');
+  var idName = null;
 
-inputGroup.find('.active').removeClass('active');
-self.addClass('active');
-idName = self.attr('href');
-$(idName).addClass('active');
+  inputGroup.find('.active').removeClass('active');
+  self.addClass('active');
+  idName = self.attr('href');
+  $(idName).addClass('active');
 });
 
 $(document).ready(function () {
